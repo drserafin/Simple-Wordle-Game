b59436da756fc98d9345ab9e2cc6a6b4cc3281d5
Kevin Serafin <kevinserafin@Kevins-MacBook-Air.local>
2024-03-18 17:56:31 -0700

    Starting all over again

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+std::cout << "Hello there , in this game you will guess a word and if you guess it right you win the game. \n";
+std::cout << "The word comes from the Bible. \n";
+std::cout << "You have 5 chances to guess the word. \n";
+
+
+
+
+
+
+    return 0;
+}
